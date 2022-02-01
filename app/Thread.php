cd2494c1bce5f57db07dccf80189ede88a202ffa
satochanpe <satoshi.muko@gmail.com>
2022-02-01 03:13:46 +0000

    投稿一覧作成

--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
--- resources/views/thread/show.blade.php
+++ resources/views/thread/show.blade.php
@@ -12,5 +12,38 @@
                 </p>
             </div>
         </div>
+        @foreach($thread->posts as $post)
+            <div class="card mt-3">
+                <div class="card-body">
+                    <h6 class="card-subtitle mb-2 text-muted">投稿者名が入ります</h6>
+                    <p class="card-text">
+                      {{ $post->body }}
+                    </p>
+                </div>
+            </div>
+        @endforeach
+        
+        <div class="card mt-3">
+            <div class="card-body">
+                <h4 class="card-title">コメントする</h4>
+                <p class="card-text">
+                    <form action="{{ action('PostController@create') }}" method="post" enctype="multipart/form-data">
+                        @if (count($errors) > 0)
+                            <ul>
+                                @foreach($errors->all() as $e)
+                                    <li>{{ $e }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
+                        <div class="form-group">
+                            <textarea class="form-control" name="body" rows="5">{{ old('body') }}</textarea>
+                        </div>
+                        <input type="hidden" name="thread_id" value={{ $thread->id }}>
+                        {{ csrf_field() }}
+                        <input type="submit" class="btn btn-primary" value="投稿">
+                    </form>
+                </p>
+            </div>
+        </div>
     </div>
 @endsection
